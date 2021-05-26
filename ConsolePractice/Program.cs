4650f16f27b68d643330de8a175af938d6e4e14c
Mursalin Kabir <mail2mursalin@gmail.com>
2021-05-27 01:33:02 +0900

    Test

--- ConsolePractice/Program.cs
+++ ConsolePractice/Program.cs
@@ -6,7 +6,7 @@ namespace ConsolePractice
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello World! My Name is Kabir");
         }
     }
 }
